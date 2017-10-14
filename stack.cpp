bf4f1082b26f542190a9eb265f4123e356e4fa1f
Shafik <shafik.te@gmail.com>
2017-10-14 07:24:22 -0700

    Dynamic Array declaration because of overflow

--- stack.cpp
+++ stack.cpp
@@ -1,12 +1,25 @@
 #include<iostream>
 using namespace std;
-#define max 100
-int top=-1; // initially top is equal to -1;
-int arr[max]; //declare a array
+int size =5;
+int top=-1; // initially top is equal to -1
+int *arr = new int[5]; // declare array pointer for dynamic array
+
+//array resizing
+void resizeArray(){
+    int *newArr = new int[size*2]; // create new array
+    for(int i=0;i<=top;i++){
+        newArr[i]= arr[i];
+    }
+    size = size*2;
+    delete[]arr;
+    arr = newArr;
+}
+
 void push ( int value){
-    if(top>=max-1){
-        cout<<"The stack is overflow"<<endl;
-        return;
+    if(top>=size-1){
+        cout<<"The stack is resizing"<<endl;
+        resizeArray();
+
     }
     arr[++top] = value;
 }
@@ -30,10 +43,10 @@ int main(){
     push(3);
     push(5);
     push(6);
-    pop();
-    pop();
-    pop();
-    pop();
+    push(2);
+    push(3);
+    push(5);
+    push(6);
     print();
 
 return 0;
